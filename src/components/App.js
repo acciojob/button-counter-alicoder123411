5b95fb5d27672fe7b9aad2034658fd2de0617639
alicoder123411 <sameersingla240@gmail.com>
2026-02-24 09:08:51 -0800

    Fix useState import and update button counter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
 const App = () => {
+  const [count,setcount]=useState(0);
+  function handleclick(){
+    setcount(count+1);
+    console.log("currently count is ",count);
+  }
   return (
     <div>
+      <h1>Button clicked clicked {count} times</h1>
+      <button onClick={handleclick}>click me</button>
         {/* Do not remove the main div */}
     </div>
   )
